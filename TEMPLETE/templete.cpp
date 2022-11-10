5d1fb5d8947d80ce2174551cabac907ebf255d61
Sumon Ahmed <85849219+Sumonsiu01@users.noreply.github.com>
2022-11-10 22:19:58 +0600

    Update template

--- TEMPLETE/templete.cpp
+++ TEMPLETE/templete.cpp
@@ -3,19 +3,23 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+#define endl '\n'
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
 #define ll long long
-
-const int mx = 1e6+123;
 #define mem(a,b) memset(a, b, sizeof(a) )
 #define endl '\n'
 #define pb push_back
 #define optimize()ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 
-bool isprime[mx];
-vector<int>prime;
+const int mx = 1e6+123;
+vector<long long>prime;
+bitset<mx>isprime;
+
+
 void primegen(int n)
 {
-    for(int i = 3;i<=n;i+=2)isprime[i] = 1;
+     n += 100;
+    for(int i = 3;i<=n;i+=2) isprime[i] = 1;
 
     int sq = sqrt(n);
 
@@ -24,27 +28,22 @@ void primegen(int n)
         if(isprime[i] == 1)
         {
             for(int j = i*i;j<=n;j+=(i+i))
-            isprime[j] = 0;
+                isprime[j] = 0;
         }
     }
-
     isprime[2] = 1;
     prime.push_back(2);
-    for(int  i = 3;i<=n;i+=2)
+
+    for(int i = 3;i<=n;i+=2)
     {
         if(isprime[i] == 1)
             prime.push_back(i);
     }
-
 }
+
 int main()
 {
-     optimize();
-
-
-
+    optimize();
 }
-
-
 
 
